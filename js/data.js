3f79c3d7d126ca0d52b15d6245ba3d0afc935714
Julian <jdaviz.11@gmail.com>
2026-06-19 00:40:38 +0200

    Pago yaz y peter Junio

--- js/data.js
+++ js/data.js
@@ -1349,7 +1349,7 @@ const participants = [
   {
     name: "Peter",
     birthday: "9 Septiembre / 2026",
-    status: "Pendiente",
+    status: "Al día",
     goal: 3000000,
     monthlyFee: 250000,
     initialPercentPaid: true,
@@ -1371,8 +1371,8 @@ const participants = [
       {
         month: "Junio",
         fee: 250000,
-        paid: 0,
-        status: "Próximo",
+        paid: 250000,
+        status: "Pagado",
       },
       {
         month: "Julio",
@@ -1769,7 +1769,7 @@ const participants = [
   {
     name: "Yaz",
     birthday: "24 Enero / 2027",
-    status: "Pendiente",
+    status: "Al día",
     goal: 3000000,
     monthlyFee: 250000,
     initialPercentPaid: true,
@@ -1791,8 +1791,8 @@ const participants = [
       {
         month: "Junio",
         fee: 250000,
-        paid: 0,
-        status: "Próximo",
+        paid: 250000,
+        status: "Pagado",
       },
       {
         month: "Julio",
