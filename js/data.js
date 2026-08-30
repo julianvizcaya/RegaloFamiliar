1aa339fde0d58a5cbebac45b71753f0440cbe91e
Julian <jdaviz.11@gmail.com>
2026-08-31 00:38:55 +0200

    Pago hugo

--- js/data.js
+++ js/data.js
@@ -506,7 +506,7 @@ const participants = [
   {
     name: "Hugo",
     birthday: "6 Septiembre / 2026",
-    status: "Pendiente",
+    status: "Al día",
     goal: 1800000,
     monthlyFee: 150000,
     initialPercentPaid: false,
@@ -540,8 +540,8 @@ const participants = [
       {
         month: "Agosto",
         fee: 150000,
-        paid: 0,
-        status: "Próximo",
+        paid: 150000,
+        status: "Pagado",
       },
       {
         month: "Septiembre",
